a3d797b3a3a0a0abebc6bb5484d714a88a3dcda7
Alberto Rosales <arosales7@ucol.mx>
2020-02-21 16:55:37 -0600

    Avance

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-import Precio from "./precio.js"
-import Producto from "./producto.js"
-import Tiempo from "./tiempo.js"
-import Fecha from "./fecha.js"
+import Precio from "./precio.js";
+import Producto from "./producto.js";
+import Tiempo from "./tiempo.js";
+import Fecha from "./fecha.js";
 
 class Main {
-    constructor(){}
-    testProducto(){
-        let x = new Producto("Mexicana", new Precio(200))
-        console.log(x.getDescripcion())
-    }
+  constructor() {}
+  testProducto() {
+    let x = new Producto("Mexicana", new Precio(200));
+    console.log(x.getDescripcion());
+  }
 }
 
-let app = new Main()
-app.testProducto()
+let app = new Main();
+app.testProducto();
