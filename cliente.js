985e6a956dd1b416f448e0a338888a94ab8dca61
Alberto Rosales <arosales7@ucol.mx>
2020-02-24 04:23:19 -0600

    Avance

---
+++ cliente.js
@@ -0,0 +1,19 @@
+import Direccion from "./direccion.js";
+
+export default class Cliente {
+    /**
+     * 
+     * @param {string} nombre | Nombre del cliente
+     * @param {Direccion} direccion | Dirección del cliente
+     * @param {number} telefono | Teléfono del cliente
+     */
+    constructor(nombre, direccion, telefono) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+    }
+
+    getPerfil() {
+        return `${this.nombre}, \n${this.direccion.getFormatoCorto()}, \nTeléfono: ${this.telefono}`;
+    }
+}
